1a1b61ef683d58b13c2beb11f2c0b447f4d27734
tapixhx <tapish818@gmail.com>
2020-08-31 18:49:02 -0700

    Hooks done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name:'Max', age:28 },
+      { name:'Max', age:28 },
+      { name:'Max', age:28 },
+    ]
+  });
+
+  const switchNameHandler = () => {
+    // console.log("Was clicked!");
+    setPersonsState({
+      persons: [
+        { name:'Maximilian', age:28 },
+        { name:'Max', age:28 },
+        { name:'Max', age:28 },
+      ] 
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi this is react app!</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies : Racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
   );
 }
